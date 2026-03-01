585b3d2869c70a815e66dded6fe71ee7137f4af7
GEO-FS-GUY <zach09700@gmail.com>
2026-03-01 13:21:07 +1100

    Create a(7).js

---
+++ a(7).js
@@ -0,0 +1,8 @@
+<script>
+  // Use a relative URL
+  var audio = new Audio('C:\Users\mzach\Downloads\Voicy_Boeing 737 autopilot disconnect alarm.mp3');
+  function playAudio() {
+    audio.play();
+  }
+</script>
+<button onclick="playAudio()">Play Sound</button>
