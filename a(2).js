30c5992ef16712a38aad8ae068717e4088b501e6
GEO-FS-GUY <zach09700@gmail.com>
2026-03-01 11:27:27 +1100

    Create a(2).js

---
+++ a(2).js
@@ -0,0 +1,8 @@
+<script>
+  // /Just-some-GPWS-sounds/
+  var audio = new Audio('C:\Users\mzach\Downloads\ap-disconnect.wav');
+  function playAudio() {
+    audio.play();
+  }
+</script>
+<button onclick="playAudio()">Play Sound</button>
