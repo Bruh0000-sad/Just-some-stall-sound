a7b5d9862fa4796d18dd207c4af16177a19c342b
GEO-FS-GUY <zach09700@gmail.com>
2026-03-01 12:58:00 +1100

    Create a(5).js

---
+++ a(5).js
@@ -0,0 +1,8 @@
+<script>
+  // /Just-some-stall-sounds
+  var audio = new Audio('C:\Users\mzach\Downloads\pmdg_777_masterwarning.mp3');
+  function playAudio() {
+    audio.play();
+  }
+</script>
+<button onclick="playAudio()">Play Sound</button>
