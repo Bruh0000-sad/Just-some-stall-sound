3d483fe3d76c0c0d68812d0eb4188987a2aafc04
GEO-FS-GUY <zach09700@gmail.com>
2026-03-01 19:40:21 +1100

    Create a(8).js

---
+++ a(8).js
@@ -0,0 +1,8 @@
+<script>
+  // /Just-some-stall-sound/
+  var audio = new Audio('C:\Users\mzach\Downloads\Boeing 737 Stall Alarm.mp3');
+  function playAudio() {
+    audio.play();
+  }
+</script>
+<button onclick="playAudio()">Play Sound</button>
