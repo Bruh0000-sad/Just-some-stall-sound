e98648c181d8105e498307330da161080720d5d6
GEO-FS-GUY <zach09700@gmail.com>
2026-03-01 11:08:14 +1100

    Create a.js

---
+++ a.js
@@ -0,0 +1,8 @@
+<script>
+  // Use a relative URL
+  var audio = new Audio('"C:\Users\mzach\OneDrive\Desktop\e (1).wav"');
+  function playAudio() {
+    audio.play();
+  }
+</script>
+<button onclick="playAudio()">Play Sound</button>
