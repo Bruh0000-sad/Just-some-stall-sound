bfc876e979fd6ea0e2dc0edc9b615379900fe430
GEO-FS-GUY <zach09700@gmail.com>
2026-03-01 19:49:39 +1100

    Create a(9).js

---
+++ a(9).js
@@ -0,0 +1,8 @@
+<script>
+  // /Just-some-stall-sound/
+  var audio = new Audio('C:\Users\mzach\Downloads\boeing-747-pull-up-400-f.mp3');
+  function playAudio() {
+    audio.play();
+  }
+</script>
+<button onclick="playAudio()">Play Sound</button>
