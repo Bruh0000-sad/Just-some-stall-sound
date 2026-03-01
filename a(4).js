45acfcc70a4196cdfed997e0421d8c87f2afe611
GEO-FS-GUY <zach09700@gmail.com>
2026-03-01 12:14:12 +1100

    Create a(4).js

---
+++ a(4).js
@@ -0,0 +1,8 @@
+<script>
+  // Use a relative URL
+  var audio = new Audio('C:\Users\mzach\Downloads\Pull Up Boeing.mp3');
+  function playAudio() {
+    audio.play();
+  }
+</script>
+<button onclick="playAudio()">Play Sound</button>
