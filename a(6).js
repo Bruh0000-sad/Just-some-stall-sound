811c9dab956d88a6f750c90e926de8530d1bf453
GEO-FS-GUY <zach09700@gmail.com>
2026-03-01 13:04:36 +1100

    Create a(6).js

---
+++ a(6).js
@@ -0,0 +1,8 @@
+<script>
+  // /Just-some-stall-sound
+  var audio = new Audio('C:\Users\mzach\Downloads\Bank Angle Boeing.mp3');
+  function playAudio() {
+    audio.play();
+  }
+</script>
+<button onclick="playAudio()">Play Sound</button>
