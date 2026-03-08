8eb1f41e07ea6004655ebb05543935dd393a7790
GEO-FS-GUY <zach09700@gmail.com>
2026-03-08 11:47:10 +1100

    Create a(12).js

---
+++ a(12).js
@@ -0,0 +1,8 @@
+<script>
+  // /Just-some-stall-sound/
+  var audio = new Audio('C:\Users\mzach\Downloads\e65b09ba-01fd-4da6-bacd-564c9c7c59ae.mp3');
+  function playAudio() {
+    audio.play();
+  }
+</script>
+<button onclick="playAudio()">Play Sound</button>
